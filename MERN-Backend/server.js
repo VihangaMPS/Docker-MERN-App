ea90245047d2cd174efdd1244bcbe7af37a3a678
MPS Vihanga <>
2024-07-27 04:26:11 +0530

    Docker Compose

--- MERN-Backend/server.js
+++ MERN-Backend/server.js
@@ -7,13 +7,15 @@ const userRoutes = require('./userRoutes');
 
 dotenv.config({path: './config.env'});
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
 const LocalDB = process.env.DATABASE_LOCAL; // Mongo Local
-mongoose.connect(LocalDB).then(() => console.log('DB Connection successful!'));
+const MongoDBURI = process.env.MONGODB_URI;
+mongoose.connect(MongoDBURI).then(() => console.log('DB Connection successful!'));
 
-app.use("/api/users", userRoutes);
+// app.use("/api/users", userRoutes);
 
 const port = process.env.PORT;
 const server = app.listen(port, () => {
